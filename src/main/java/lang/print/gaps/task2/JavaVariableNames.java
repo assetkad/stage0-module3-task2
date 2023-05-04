339acb3bafbc822ff9c409331b48389e1efb0be6
Asset Kad <asset_kad@mail.ru>
2023-05-04 16:49:07 +0500

    finished stage 0, module #3, task#2

--- src/main/java/lang/print/gaps/task2/JavaVariableNames.java
+++ src/main/java/lang/print/gaps/task2/JavaVariableNames.java
@@ -1,14 +1,16 @@
 package lang.print.gaps.task2;
 
 public class JavaVariableNames {
-    public static void main(String[] args) {
-        int 2morrow = 0;
-        int $money = 10;
-        int %hundred = 100;
-        int !back = 1000;
-        int _underscore = 10000;
-        int #sharp = 100000;
-        int *star = 1000000;
-        int BADEXAMPLEOFVARNAME = 1000000;
-    }
+  public static void main(String[] args) {
+    int tomorrow = 0;
+    int $money = 10;
+    int onehundred = 100;
+    int back = 1000;
+    int _underscore = 10000;
+    int CSsharp = 100000;
+    int star = 1000000;
+    int badExampleOfVarName = 1000000;
+    System.out.println(tomorrow + " " + $money + " " + onehundred + " " + back + " " + _underscore + " " + CSsharp + " "
+        + star + " " + badExampleOfVarName);
+  }
 }
